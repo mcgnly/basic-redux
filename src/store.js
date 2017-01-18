d837eae364d4ebc9e9da7c5b1e6fcccd454f7203
Katie McGinley <katie@mcgnly.com>
2017-01-18 16:44:21 +0100

    configure devtools

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
@@ -7,11 +7,14 @@ const loggerMiddleware = createLogger();
 
 //createStore connects the reducers, which output state, to the actions which interact with them, 
 	//and passes the state on to the Provider in the index.js
-let store = createStore(rootReducer,
-	applyMiddleware(
-		thunkMiddleware,//thunk is what allows the actions to return functions instead of only objects, and handles async
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer,
+	composeEnhancers(
+    applyMiddleware(
+    	thunkMiddleware,//thunk is what allows the actions to return functions instead of only objects, and handles async
 		loggerMiddleware
-	)
+		)
+    )
 );
 
 export default store;
